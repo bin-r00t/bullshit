31e77a10cc5d217027a28c4138e9f8f617cb42f5
bin-r00t <bh0bama@sina.com>
2023-10-06 14:19:36 +0800

    add footer

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+// import { NavLink } from "react-router-dom";
+// import SearchBar from "../components/SearchBar.jsx";
+
+function Footer() {
+  return <footer className="mt-10 border-t ">
+    <div className="container mx-auto py-10">
+      hello
+    </div>
+  </footer>;
+}
+
+export default Footer;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
-function Layout () {
-    return (
-        <div className="h-screen flex flex-col">
-            <Header>header</Header>
-            <Outlet />
-        </div>
-    )
-};
+import Footer from "./Footer";
+
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header>header</Header>
+      <main className="h-1 flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 export default Layout;
